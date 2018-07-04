b60ca54484b6544d7040e8e3f456162ad5a091d5
Marika Ross <marika.ross@gmail.com>
2018-07-04 16:49:21 -0600

    Refactor map in CardContainer

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Card = (props) => {
+const Card = ({name, homeworld, species, population}) => {
+
   return (
-    <article className='card'>
-      <h2>{props.name}</h2>
-      <h3>homeworld: {props.homeworld}</h3>
-      <h3>species: {props.species}</h3>
-      <h3>population: {props.population}</h3>
+    <article className='card' key={name}>
+      <h2>{name}</h2>
+      <h3>homeworld: {homeworld}</h3>
+      <h3>species: {species}</h3>
+      <h3>population: {population}</h3>
     </article>
     )
 }
--- src/Components/CardContainer/CardContainer.js
+++ src/Components/CardContainer/CardContainer.js
@@ -3,13 +3,7 @@ import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
 
 const CardContainer = ({cards}) => {
-  const allCards = cards.map(card => {
-    return <Card 
-      name={name}
-      homeworld={homeworld}
-      species={species}
-      population={population} />
-  })
+  const allCards = cards.map(card => <Card key={card.name} {...card}/>);
   return (
     <div>
       {allCards}

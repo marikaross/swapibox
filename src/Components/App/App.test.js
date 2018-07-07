e7095a9b85d64c5d523d508952442302563e0311
Marika Ross <marika.ross@gmail.com>
2018-07-07 12:14:14 -0600

    Create more failing tests in api-caller and app

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -10,13 +10,33 @@ jest.mock('../helper/api-caller.js');
 describe('App component', () => {
 
 
-  // it('getPeople calls findPeople and sets the state.cards', async () => {
-  //   let wrapper = shallow(<App />);
+  it('getPeople calls findPeople and sets the state.cards', async () => {
+    let wrapper = shallow(<App />);
     
-  //   await wrapper.instance().getPeople();
+    await wrapper.instance().getPeople();
 
-  //   expect(findPeople).toHaveBeenCalled();
-  //   // expect(wrapper.state('cards')).toEqual()
-  // });
+    expect(findPeople).toHaveBeenCalled();
+    expect(wrapper.state('cards')).toEqual()
+  });
+
+  it('after getPeople is called that state should be set', async () => {
+    const expected = [{name: 'Bingo', 
+      homeworld: 'farm', 
+      species: 'canine', 
+      population: 15 },
+      {name: 'Wanda',
+      homeworld: 'fish-bowl',
+      species: 'fish',
+      population: 1}
+    ]
+      wrapper.setState({ cards: expected })
+
+      // Execution
+      await wrapper.instance().getPeople()
+      
+      // Expectation
+      expect(wrapper.state('cards')).toEqual(expected)
+  })
+
  
 });
